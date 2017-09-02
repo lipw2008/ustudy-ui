2f2b153f9ca485faad5c7d756a8bf6ca40262472
leekiea <17413021@qq.com>
2017-09-02 20:51:58 +0800

    add logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ustudy-app',
@@ -8,10 +9,31 @@ export class AppComponent {
     pageTitle: string = '蘑菇云后台管理系统';
     userName: string = '';
 
+	constructor(private router: Router) {
+
+	}
+
     ngOnInit() : void {
         console.log("get user name...");
 		this.getUserName();
     }
+
+	logout(): void {
+		const req = new XMLHttpRequest();
+		req.open('GET', 'http://47.92.53.57:8080/info/logout');
+		req.setRequestHeader("Content-type", "application/json");
+		var that = this;
+		req.onreadystatechange = function() {
+			if (req.readyState == 4 && req.status/100 == 2) {
+				alert("您已退出");
+				that.router.navigate(['welcome']);
+			} else if (req.readyState == 4 && req.status/100 != 2) {
+				alert("退出失败！");
+				that.router.navigate(['welcome']);
+			}
+		}
+		req.send();
+	}
 
     getUserName() {
 		this.fetch((data) => {
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component }  from '@angular/core';
+import { Component, OnInit }  from '@angular/core';
 
 
 @Component({
@@ -7,4 +7,27 @@ import { Component }  from '@angular/core';
 
 export class WelcomeComponent{
 
+    role: string = "任课老师";
+
+    ngOnInit() {
+        this.getUserName();
+    }
+
+    getUserName() {
+		this.fetch((data) => {
+			//cache the list
+			console.log("data: " + data);
+			//this.userName = data===undefined ? '' : data;
+		});		
+	}
+	
+	fetch(cb) {
+		const req = new XMLHttpRequest();
+		req.open('GET', 'http://47.92.53.57:8080/info/loginId');
+		req.onload = () => {
+			cb(req.response);
+		};
+		
+		req.send();
+	}
 }
