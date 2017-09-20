4472828860460e728ac77658c67909d03db2ee16
leekiea <17413021@qq.com>
2017-09-20 16:55:06 +0800

    refine exam page

--- src/app/examCenter/mark/mark.component.ts
+++ src/app/examCenter/mark/mark.component.ts
@@ -16,6 +16,10 @@ export class MarkComponent implements OnInit {
 	cxt: any;
 	img: any;
 
+	// hidden canvas
+	hCanvas: any;
+	hCxt: any;
+
 	editMode: string = 'None';
 
 	// Draw Line
@@ -57,6 +61,10 @@ export class MarkComponent implements OnInit {
 
 		this.cxt = this.canvas.getContext("2d");
 
+		// hidden canvas
+		this.hCanvas = document.createElement("canvas");
+		this.hCxt = this.hCanvas.getContext("2d");
+
 		this.img = new Image();
 
 		let t = this;
@@ -64,15 +72,20 @@ export class MarkComponent implements OnInit {
 			// If the size of image is small, scale it to fit the container.
 			console.log("image width:" + t.img.width + " container.width:" + t.container.style.width);
 			// TODO: change 800 to real time container width later.
-			if (t.img.width < 800) {
-				t.cxt.canvas.width = 800;
-				t.cxt.canvas.height = t.img.height * (800/t.img.width);
+			// if (t.img.width < 800) {
+				t.cxt.canvas.width = 780;
+				t.cxt.canvas.height = t.img.height * (780/t.img.width);
 				t.cxt.drawImage(t.img, 0, 0, t.img.width, t.img.height, 0, 0, t.cxt.canvas.width, t.cxt.canvas.height);
-			} else {
-				t.cxt.canvas.width = t.img.width;
-				t.cxt.canvas.height = t.img.height;
-				t.cxt.drawImage(t.img, 0, 0, t.img.width, t.img.height);				
-			}
+
+				//hidden canvas
+				t.hCxt.canvas.width = t.cxt.canvas.width;
+				t.hCxt.canvas.height = t.cxt.canvas.height;
+
+			// } else {
+			// 	t.cxt.canvas.width = t.img.width;
+			// 	t.cxt.canvas.height = t.img.height;
+			// 	t.cxt.drawImage(t.img, 0, 0, t.img.width, t.img.height);				
+			// }
 		}
 		this.img.src = 'assets/api/exams/exam01.png';
 	}
@@ -81,13 +94,22 @@ export class MarkComponent implements OnInit {
 		switch(this.editMode) {
 			case 'Line':
 				this.cxt.beginPath();
-				this.cxt.moveTo(evt.offsetX, evt.offsetY);
+				this.cxt.moveTo(evt.offsetX == undefined? evt.layerX: evt.offsetX, evt.offsetY == undefined? evt.layerY: evt.offsetY);
+				
+				//hidden canvas
+				this.hCxt.beginPath();
+				this.hCxt.moveTo(evt.offsetX == undefined? evt.layerX: evt.offsetX, evt.offsetY == undefined? evt.layerY: evt.offsetY);
+				
 				this.isDrawingLine = true;
 				break;
 			case 'Circle':
 				this.cxt.beginPath();
-				this.circleBeginX = evt.offsetX;
-				this.circleBeginY = evt.offsetY;
+
+				//hidden canvas
+				this.hCxt.beginPath();
+
+				this.circleBeginX = evt.offsetX == undefined? evt.layerX: evt.offsetX;
+				this.circleBeginY = evt.offsetY == undefined? evt.layerY: evt.offsetY;
 				break;
 			case 'Text':
 				let textbox = document.createElement("input");
@@ -103,16 +125,28 @@ export class MarkComponent implements OnInit {
     					console.log(textbox.value);
     					this.cxt.font = '36px serif';
     					this.cxt.fillStyle = "red";
-    					this.cxt.fillText(textbox.value, tEvt.offsetX, tEvt.offsetY + 36);
+    					this.cxt.fillText(textbox.value, (tEvt.offsetX == undefined || tEvt.offsetX == 0) ? tEvt.layerX: tEvt.offsetX, 
+						((tEvt.offsetY == undefined || tEvt.offsetY == 0) ? tEvt.layerY: tEvt.offsetY) + 36);
+
+						//hidden canvas
+    					this.hCxt.font = '36px serif';
+    					this.hCxt.fillStyle = "red";
+    					this.hCxt.fillText(textbox.value, (tEvt.offsetX == undefined || tEvt.offsetX == 0) ? tEvt.layerX: tEvt.offsetX, 
+						((tEvt.offsetY == undefined || tEvt.offsetY == 0) ? tEvt.layerY: tEvt.offsetY) + 36);
+
+						console.log(tEvt.layerX);
+						console.log(tEvt.offsetX);
 						textbox.parentNode.removeChild(textbox);
 					}
     			});
 				this.container.appendChild(textbox);
 				textbox.focus();
 				console.log(textbox);
-				console.log(this.canvas.offsetLeft + ' ' + evt.offsetX);
+				// console.log(this.canvas.offsetLeft);
+				// console.log(evt.offsetX == undefined? evt.layerX: evt.offsetX);
 				break;
 			default:
+				console.log(evt.layerY);
 				//Do Nothing
 		}
 	}
@@ -122,40 +156,70 @@ export class MarkComponent implements OnInit {
 			case 'Line':
 				if (this.isDrawingLine === true) {
 					this.isDrawingLine = false;
-					this.cxt.lineTo(evt.offsetX, evt.offsetY);
+					this.cxt.lineTo(evt.offsetX == undefined? evt.layerX: evt.offsetX, evt.offsetY == undefined? evt.layerY: evt.offsetY);
 					this.cxt.lineWidth = 3;
 					this.cxt.strokeStyle = "red";
 					this.cxt.stroke();
 					this.cxt.closePath();
+
+					//hidden canvas
+					this.hCxt.lineTo(evt.offsetX == undefined? evt.layerX: evt.offsetX, evt.offsetY == undefined? evt.layerY: evt.offsetY);
+					this.hCxt.lineWidth = 3;
+					this.hCxt.strokeStyle = "red";
+					this.hCxt.stroke();
+					this.hCxt.closePath();
+					
 				}
 				break;
 			case 'Circle':
 				let circleEndX = 0;
 				let circleEndY = 0;
 				// Deal with drawing from right to left
-				if (this.circleBeginX > evt.offsetX) {
+				if (this.circleBeginX > (evt.offsetX == undefined? evt.layerX: evt.offsetX)) {
 					circleEndX = this.circleBeginX;
-					this.circleBeginX = evt.offsetX;
+					this.circleBeginX = evt.offsetX == undefined? evt.layerX: evt.offsetX;
 				} else {
-					circleEndX = evt.offsetX;
+					circleEndX = evt.offsetX == undefined? evt.layerX: evt.offsetX;
 				}
 				// Deal with drawing from bottom to top
-				if (this.circleBeginY > evt.offsetY) {
+				if (this.circleBeginY > (evt.offsetY == undefined? evt.layerY: evt.offsetY)) {
 					circleEndY = this.circleBeginY;
-					this.circleBeginY = evt.offsetY;
+					this.circleBeginY = evt.offsetY == undefined? evt.layerY: evt.offsetY;
 				} else {
-					circleEndY = evt.offsetY;
+					circleEndY = evt.offsetY == undefined? evt.layerY: evt.offsetY;
 				}
 				let calX = circleEndX - this.circleBeginX;
 				let calY = circleEndY - this.circleBeginY;
 				let r = Math.pow(calX * calX + calY * calY, 0.5)/2;
+				let a = r;
+				let b = r/2;
 				let centerX = this.circleBeginX + calX/2;
 				let centerY = this.circleBeginY + calY/2;
-				this.cxt.arc(centerX, centerY, r, 0, 2*Math.PI);
+				let radioX = a/r;
+				let radioY = b/r;
+				this.cxt.save();
+				this.cxt.scale(radioX, radioY);
+				this.cxt.beginPath();
 				this.cxt.lineWidth = 3;
 				this.cxt.strokeStyle = "red";
-				this.cxt.stroke();
+				this.cxt.moveTo((centerX+a)/radioX, centerY/radioY);
+				this.cxt.arc(centerX/radioX, centerY/radioY, r, 0, 2*Math.PI);
 				this.cxt.closePath();
+				this.cxt.stroke();
+				this.cxt.restore();
+
+				// hidden canvas
+				this.hCxt.save();
+				this.hCxt.scale(radioX, radioY);
+				this.hCxt.beginPath();
+				this.hCxt.lineWidth = 3;
+				this.hCxt.strokeStyle = "red";
+				this.hCxt.moveTo((centerX+a)/radioX, centerY/radioY);
+				this.hCxt.arc(centerX/radioX, centerY/radioY, r, 0, 2*Math.PI);
+				this.hCxt.closePath();
+				this.hCxt.stroke();
+				this.hCxt.restore();
+
 				break;
 			default:
 				//Do Nothing
@@ -198,5 +262,19 @@ export class MarkComponent implements OnInit {
 	saveImage(): void {
 		let dataUrl = this.canvas.toDataURL("image/png");
 		window.open(dataUrl);
+		let hDataUrl = this.hCanvas.toDataURL("image/png");
+		window.open(hDataUrl);
+	}
+
+	addImage(): void {
+		this.img = new Image();
+		let t = this;
+		this.img.onload = function() {
+			// If the size of image is small, scale it to fit the container.
+			console.log("image width:" + t.img.width + " container.width:" + t.container.style.width);
+			// TODO: change 800 to real time container width later.
+			t.cxt.drawImage(t.img, 0, 150, t.img.width, t.img.height-150, 0, 150, t.cxt.canvas.width, t.cxt.canvas.height-150);
+		}
+		this.img.src = 'assets/api/exams/hidden.png';
 	}
 }
