3b2435c9c3484c2f28356d1612907ff32442de4e
dulei2085 <645744938@qq.com>
2017-09-21 08:07:23 +0800

    update

--- src/app/examCenter/setanswers/selectsubject.component.ts
+++ src/app/examCenter/setanswers/selectsubject.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit }  from '@angular/core';
+import { Component, OnInit, ElementRef }  from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import {ActivatedRoute, Router} from '@angular/router';
 import { SharedService } from '../../shared.service';
 
 @Component({
@@ -9,89 +9,118 @@ import { SharedService } from '../../shared.service';
 
 export class SelectSubjectComponent implements OnInit {
 
-	public searchForm = this.fb.group({
-		examName: [""],
-		grade: [""],
-		subject: [""],
-		startDate: [""],
-		endDate: [""]
-	});
-	
-    errorMessage: string;
+	errorMessage: string;
+	examId: string;
 
 	exams = [
-		{ id: '1', name: '铜川一中16-17学年上学期期末考试' },
-		{ id: '2', name: '铜川一中16-17学年上学期期末考试' },
-		{ id: '3', name: '铜川一中16-17学年上学期期末考试' },
-		{ id: '4', name: '铜川一中16-17学年上学期期末考试' },
-		{ id: '5', name: '铜川一中16-17学年上学期期末考试' },
-		{ id: '6', name: '铜川一中16-17学年上学期期末考试' }
+		{ id: '1', name: '铜川一中16-17学年上学期期末考试1' },
+		{ id: '2', name: '铜川一中16-17学年上学期期末考试2' },
+		{ id: '3', name: '铜川一中16-17学年上学期期末考试3' },
+		{ id: '4', name: '铜川一中16-17学年上学期期末考试4' },
+		{ id: '5', name: '铜川一中16-17学年上学期期末考试5' },
+		{ id: '6', name: '铜川一中16-17学年上学期期末考试6' }
 	];
 
 	setedsubjects = [
-		{ grade: '高一', subjects: ["数学", "物理", "化学"]},
-		{ grade: '高二', subjects: ["数学", "物理", "化学"]},
-		{ grade: '高三', subjects: ["数学", "物理", "化学"]}
+		{ id:'10', grade: '高一', subjects: [
+			{id: '1', name: '数学'},
+			{id: '2', name: '物理'},
+			{id: '3', name: '化学'}
+		]},
+		{ id:'11', grade: '高二', subjects: [
+			{id: '1', name: '数学'},
+			{id: '2', name: '物理'},
+			{id: '3', name: '化学'}
+		]},
+		{ id:'12', grade: '高三', subjects: [
+			{id: '1', name: '数学'},
+			{id: '2', name: '物理'},
+			{id: '3', name: '化学'}
+		]}
 	];
 
 	unsetedsubjects = [
-		{ grade: '高一', subjects: ["语文", "英语", "生物", "政治", "地理", "历史"]},
-		{ grade: '高二', subjects: ["语文", "英语", "生物", "政治", "地理", "历史"]},
-		{ grade: '高三', subjects: ["语文", "英语", "生物", "政治", "地理", "历史"]}
+		{ id:'10', grade: '高一', subjects: [
+			{id: '4', name: '语文'},
+			{id: '5', name: '英语'},
+			{id: '6', name: '生物'},
+			{id: '4', name: '政治'},
+			{id: '5', name: '地理'},
+			{id: '6', name: '历史'}
+		]},
+		{ id:'11', grade: '高二', subjects: [
+			{id: '4', name: '语文'},
+			{id: '5', name: '英语'},
+			{id: '6', name: '生物'},
+			{id: '4', name: '政治'},
+			{id: '5', name: '地理'},
+			{id: '6', name: '历史'}]},
+		{ id:'12', grade: '高三', subjects: [
+			{id: '4', name: '语文'},
+			{id: '5', name: '英语'},
+			{id: '6', name: '生物'},
+			{id: '4', name: '政治'},
+			{id: '5', name: '地理'},
+			{id: '6', name: '历史'}]}
 	];
 
-    constructor(private _sharedService: SharedService, public fb: FormBuilder) {
+    constructor(private _sharedService: SharedService, public fb: FormBuilder, private elementRef: ElementRef, private route: ActivatedRoute, private router: Router) {
 
     }
 
     ngOnInit(): void {
-		this.reload();
+		this.loadExams();
+	}
+
+	loadExams() {
+
 	}
 	
-	reload() {
-		//req.open('GET', 'assets/api/teachers/teachers.json');
-		this._sharedService.makeRequest('GET', 'assets/api/exams/exams.json', '').then((data: any) => {
-			//cache the list
-			console.log("data: " + JSON.stringify(data));
-			for(var t of data) {
-				//科目
-				if (t.subjects && t.subjects.length >0) {
-					var str = "";
-					for (var s of t.subjects) {
-						str += s.n + " ";
-					}
-					t.subjects = str;
-				} else {
-					t.subjects = "";
-				}	
-				//年级
-				if (t.grades && t.grades.length >0) {
-					var str = "";
-					for (var g of t.grades) {
-						str += g.n + " ";
-					}
-					t.grades = str;
-				} else {
-					t.grades = "";
-				}							
-			}
-		}).catch((error: any) => {
-			console.log(error.status);
-			console.log(error.statusText);
-		});
-	}
-	
-	filter(event) {
-		// filter our data
-		// var t = this;
-		// const temp = this.temp.filter(function(d) {
-		// 	return d.grades.indexOf(t.grade) !== -1 
-		// 	&& d.subjects.indexOf(t.subject) !== -1
-		// 	&& d.examName.indexOf(t.examName) !== -1;
+	reload(examId) {
+		// //req.open('GET', 'assets/api/teachers/teachers.json');
+		// this._sharedService.makeRequest('GET', 'assets/api/exams/exams.json', '').then((data: any) => {
+		// 	//cache the list
+		// 	console.log("data: " + JSON.stringify(data));
+		// 	for(var t of data) {
+		// 		//科目
+		// 		if (t.subjects && t.subjects.length >0) {
+		// 			var str = "";
+		// 			for (var s of t.subjects) {
+		// 				str += s.n + " ";
+		// 			}
+		// 			t.subjects = str;
+		// 		} else {
+		// 			t.subjects = "";
+		// 		}	
+		// 		//年级
+		// 		if (t.grades && t.grades.length >0) {
+		// 			var str = "";
+		// 			for (var g of t.grades) {
+		// 				str += g.n + " ";
+		// 			}
+		// 			t.grades = str;
+		// 		} else {
+		// 			t.grades = "";
+		// 		}							
+		// 	}
+		// }).catch((error: any) => {
+		// 	console.log(error.status);
+		// 	console.log(error.statusText);
 		// });
-		// update the rows
-		// this.rows = temp;
-		// Whenever the filter changes, always go back to the first page
-		//this.table.offset = 0;
+	}
+
+	getExam() {
+		const examId = this.elementRef.nativeElement.querySelector('#examFilterValue').value;
+		this.examId = examId;
+		if(examId !== "0"){
+			this.reload(examId);
+			this.elementRef.nativeElement.querySelector('#editGradeDetailsForm').style.display = '';
+		}else{
+			this.elementRef.nativeElement.querySelector('#editGradeDetailsForm').style.display = 'none';
+		}
+	}	
+
+	setAnswers(gradeId,subjectId,seted) {
+		this.router.navigate(['setanswers', {examId: this.examId, gradeId: gradeId, subjectId: subjectId, seted: seted}]);
 	}
 }
--- src/app/examCenter/setanswers/setanswers.component.ts
+++ src/app/examCenter/setanswers/setanswers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit }  from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import {ActivatedRoute, Router} from '@angular/router';
 import { SharedService } from '../../shared.service';
 
 @Component({
@@ -9,95 +9,126 @@ import { SharedService } from '../../shared.service';
 
 export class SetAnswersComponent implements OnInit {
 
-	public searchForm = this.fb.group({
-		examName: [""],
-		grade: [""],
-		subject: [""],
-		startDate: [""],
-		endDate: [""]
-	});
-	
-    errorMessage: string;
+	errorMessage: string;
 
-	grades = [];
-	
-	subjects = [];
-	
-	rows = [];
-	
-	temp = [];
-	
-	columns = [
-		{ prop: 'examName', name: '考试名称' },
-		{ prop: 'startDate', name: '开考时间' },
-		{ prop: 'grades', name: '考试年级' },
-		{ prop: 'subjects', name: '考试科目' },
-		{ prop: 'examineeNum', name: '考生人数' }
+	examId: string;
+	gradeId: string;
+	subjectId: string;
+	seted: boolean;	
+
+	objective = [
+		{start:1,end:20,type:1,option:4,score:1},
+		{start:21,end:25,type:2,option:4,score:2},
+		{start:26,end:30,type:3,option:2,score:2}
 	];
-	
-	// filter keys:
-	grade = "";
-	subject = "";
-	startDate = "";
-	endDate = "";
-	examName = "";
+	objectiveAnswers = [
+		{no:1,answer:'A',subject:0},
+		{no:2,answer:'B',subject:0},
+		{no:3,answer:'C',subject:0},
+		{no:4,answer:'D',subject:0},
+		{no:5,answer:'A',subject:0},
+		{no:6,answer:'B',subject:0},
+		{no:7,answer:'C',subject:0},
+		{no:8,answer:'D',subject:0},
+		{no:9,answer:'A',subject:0},
+		{no:10,answer:'B',subject:10},
+		{no:11,answer:'A',subject:11},
+		{no:12,answer:'B',subject:12},
+		{no:13,answer:'C',subject:11},
+		{no:14,answer:'D',subject:10},
+		{no:15,answer:'A',subject:12},
+		{no:16,answer:'B',subject:0},
+		{no:17,answer:'C',subject:0},
+		{no:18,answer:'D',subject:0},
+		{no:19,answer:'A',subject:0},
+		{no:20,answer:'B',subject:0},
+		{no:21,answer:'AB',subject:0},
+		{no:22,answer:'BC',subject:0},
+		{no:23,answer:'CD',subject:0},
+		{no:24,answer:'DC',subject:0},
+		{no:25,answer:'AD',subject:0},
+		{no:26,answer:'0',subject:0},
+		{no:27,answer:'0',subject:0},
+		{no:28,answer:'1',subject:0},
+		{no:29,answer:'0',subject:0},
+		{no:30,answer:'1',subject:0}
+	];
+	subjective = [
+		{type:4,start:31,end:35,subject:0,score:2},
+		{type:4,start:36,subject:0,score:10},
+		{type:4,start:37,subject:0,score:10,child:[
+			{no:1,type:4,subject:0,score:5},
+			{no:2,type:4,subject:0,score:5}
+		]},
+		{type:4,start:38,subject:10,score:10,child:[
+			{no:1,type:4,subject:10,score:5},
+			{no:2,type:4,subject:10,score:5}
+		]},
+		{type:4,start:39,subject:0,score:20,child:[
+			{no:1,type:4,subject:10,score:6},
+			{no:2,type:4,subject:11,score:6},
+			{no:3,type:4,subject:12,score:8}
+		]}
+	];
 
-    constructor(private _sharedService: SharedService, public fb: FormBuilder) {
+	constructor(private _sharedService: SharedService, public fb: FormBuilder, private route: ActivatedRoute, private router: Router) {
 
     }
 
     ngOnInit(): void {
-		this.reload();
-        this.grades = ["高一", "高二", "高三"];
-        this.subjects = ["数学", "物理", "化学"];
+		this.examId = this.route.snapshot.params.examId;
+		this.gradeId = this.route.snapshot.params.gradeId;
+		this.subjectId = this.route.snapshot.params.subjectId;
+		this.seted = this.route.snapshot.params.seted;
+
+		if(this.seted){
+			this.reload(this.examId, this.gradeId, this.subjectId);
+		}
+
+		this.initpage();
+	}
+
+	//根據答案設置信息初始化頁面
+	initpage(){
+
 	}
 	
-	reload() {
-		//req.open('GET', 'assets/api/teachers/teachers.json');
-		this._sharedService.makeRequest('GET', 'assets/api/exams/exams.json', '').then((data: any) => {
-			//cache the list
-			console.log("data: " + JSON.stringify(data));
-			for(var t of data) {
-				//科目
-				if (t.subjects && t.subjects.length >0) {
-					var str = "";
-					for (var s of t.subjects) {
-						str += s.n + " ";
-					}
-					t.subjects = str;
-				} else {
-					t.subjects = "";
-				}	
-				//年级
-				if (t.grades && t.grades.length >0) {
-					var str = "";
-					for (var g of t.grades) {
-						str += g.n + " ";
-					}
-					t.grades = str;
-				} else {
-					t.grades = "";
-				}							
-			}
-			this.temp = [...data];
-			this.rows = data;
-		}).catch((error: any) => {
-			console.log(error.status);
-			console.log(error.statusText);
-		});
+	reload(examId,gradeId,subjectId) {
+		// //req.open('GET', 'assets/api/teachers/teachers.json');
+		// this._sharedService.makeRequest('GET', 'assets/api/exams/exams.json', '').then((data: any) => {
+		// 	//cache the list
+		// 	console.log("data: " + JSON.stringify(data));
+		// 	for(var t of data) {
+		// 		//科目
+		// 		if (t.subjects && t.subjects.length >0) {
+		// 			var str = "";
+		// 			for (var s of t.subjects) {
+		// 				str += s.n + " ";
+		// 			}
+		// 			t.subjects = str;
+		// 		} else {
+		// 			t.subjects = "";
+		// 		}	
+		// 		//年级
+		// 		if (t.grades && t.grades.length >0) {
+		// 			var str = "";
+		// 			for (var g of t.grades) {
+		// 				str += g.n + " ";
+		// 			}
+		// 			t.grades = str;
+		// 		} else {
+		// 			t.grades = "";
+		// 		}							
+		// 	}
+		// 	this.temp = [...data];
+		// 	this.rows = data;
+		// }).catch((error: any) => {
+		// 	console.log(error.status);
+		// 	console.log(error.statusText);
+		// });
 	}
 	
-	filter(event) {
-		// filter our data
-		var t = this;
-		const temp = this.temp.filter(function(d) {
-			return d.grades.indexOf(t.grade) !== -1 
-			&& d.subjects.indexOf(t.subject) !== -1
-			&& d.examName.indexOf(t.examName) !== -1;
-		});
-		// update the rows
-		this.rows = temp;
-		// Whenever the filter changes, always go back to the first page
-		//this.table.offset = 0;
+	addOne(){
+		
 	}
 }
