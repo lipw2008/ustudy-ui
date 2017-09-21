d5ee2ced2f5d708cd0694cf716ec62d79ce157c2
dulei2085 <645744938@qq.com>
2017-09-22 00:02:51 +0800

    update

--- src/app/examCenter/setanswers/setanswers.component.ts
+++ src/app/examCenter/setanswers/setanswers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit }  from '@angular/core';
+import { Component, OnInit, ElementRef }  from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import { SharedService } from '../../shared.service';
@@ -16,44 +16,41 @@ export class SetAnswersComponent implements OnInit {
 	subjectId: string;
 	seted: boolean;	
 
-	objective = [
-		{start:1,end:20,type:1,option:4,score:1},
-		{start:21,end:25,type:2,option:4,score:2},
-		{start:26,end:30,type:3,option:2,score:2}
+	options = [2,3,4,5,6,7,8,9,10];
+
+	objectives = [
+		{id:1, start:1,end:20,type:1,option:4,score:1}
 	];
+
 	objectiveAnswers = [
-		{no:1,answer:'A',subject:0},
-		{no:2,answer:'B',subject:0},
-		{no:3,answer:'C',subject:0},
-		{no:4,answer:'D',subject:0},
-		{no:5,answer:'A',subject:0},
-		{no:6,answer:'B',subject:0},
-		{no:7,answer:'C',subject:0},
-		{no:8,answer:'D',subject:0},
-		{no:9,answer:'A',subject:0},
-		{no:10,answer:'B',subject:10},
-		{no:11,answer:'A',subject:11},
-		{no:12,answer:'B',subject:12},
-		{no:13,answer:'C',subject:11},
-		{no:14,answer:'D',subject:10},
-		{no:15,answer:'A',subject:12},
-		{no:16,answer:'B',subject:0},
-		{no:17,answer:'C',subject:0},
-		{no:18,answer:'D',subject:0},
-		{no:19,answer:'A',subject:0},
-		{no:20,answer:'B',subject:0},
-		{no:21,answer:'AB',subject:0},
-		{no:22,answer:'BC',subject:0},
-		{no:23,answer:'CD',subject:0},
-		{no:24,answer:'DC',subject:0},
-		{no:25,answer:'AD',subject:0},
-		{no:26,answer:'0',subject:0},
-		{no:27,answer:'0',subject:0},
-		{no:28,answer:'1',subject:0},
-		{no:29,answer:'0',subject:0},
-		{no:30,answer:'1',subject:0}
+		{no:1,type:1,option:4,answer:'A',subject:0},
+		{no:2,type:1,option:4,answer:'B',subject:0},
+		{no:3,type:1,option:4,answer:'C',subject:0},
+		{no:4,type:1,option:4,answer:'D',subject:0},
+		{no:5,type:1,option:4,answer:'A',subject:0},
+		{no:6,type:1,option:4,answer:'B',subject:0},
+		{no:7,type:1,option:4,answer:'C',subject:0},
+		{no:8,type:1,option:4,answer:'D',subject:0},
+		{no:9,type:1,option:4,answer:'A',subject:0},
+		{no:10,type:1,option:4,answer:'B',subject:10},
+		{no:11,type:1,option:4,answer:'A',subject:11},
+		{no:12,type:1,option:4,answer:'B',subject:12},
+		{no:13,type:1,option:4,answer:'C',subject:11},
+		{no:14,type:1,option:4,answer:'D',subject:10},
+		{no:15,type:1,option:4,answer:'A',subject:12},
+		{no:16,type:1,option:4,answer:'B',subject:0},
+		{no:17,type:1,option:4,answer:'C',subject:0},
+		{no:18,type:1,option:4,answer:'D',subject:0},
+		{no:19,type:1,option:4,answer:'A',subject:0},
+		{no:20,type:1,option:4,answer:'B',subject:0}
 	];
-	subjective = [
+
+	radioScore = 20;
+	checkboxScore = 0;
+	judgmentScore = 0;
+	objectiveScore = 20;
+
+	subjectives = [
 		{type:4,start:31,end:35,subject:0,score:2},
 		{type:4,start:36,subject:0,score:10},
 		{type:4,start:37,subject:0,score:10,child:[
@@ -71,7 +68,7 @@ export class SetAnswersComponent implements OnInit {
 		]}
 	];
 
-	constructor(private _sharedService: SharedService, public fb: FormBuilder, private route: ActivatedRoute, private router: Router) {
+	constructor(private _sharedService: SharedService, public fb: FormBuilder, private elementRef: ElementRef, private route: ActivatedRoute, private router: Router) {
 
     }
 
@@ -84,13 +81,6 @@ export class SetAnswersComponent implements OnInit {
 		if(this.seted){
 			this.reload(this.examId, this.gradeId, this.subjectId);
 		}
-
-		this.initpage();
-	}
-
-	//根據答案設置信息初始化頁面
-	initpage(){
-
 	}
 	
 	reload(examId,gradeId,subjectId) {
@@ -128,7 +118,139 @@ export class SetAnswersComponent implements OnInit {
 		// });
 	}
 	
-	addOne(){
-		
+	addOneRow(){
+		if(this.objectives.length > 0){
+			const obj = this.objectives[this.objectives.length-1];
+			const obj_ = {id:new Date().getTime(), start:1,end:20,type:1,option:4,score:1};
+			obj_['start'] = obj['end'] + 1;
+			obj_['end'] = obj_['start'];
+			this.objectives.push(obj_);
+	
+			this.addScore(obj_);
+			this.addAnswers(obj_);
+		}else{
+			const obj = {id:1, start:1,end:20,type:1,option:4,score:1};
+			this.objectives.push(obj);
+
+			this.addScore(obj);
+			this.addAnswers(obj);
+		}
 	}
+
+	removeOneRow(id){
+		const _objectives = [];
+		for(var i=0;i<this.objectives.length;i++){
+			const objective = this.objectives[i];
+			if(objective && objective['id'] !== id){
+				_objectives.push(objective);
+			}else{
+				this.removeAnswers(objective)
+				this.removeScore(objective);
+			}
+		}
+		this.objectives = _objectives;
+	}
+
+	addAnswers(objective){
+		let start = objective['start'];
+		let end = objective['end'];
+		let type = objective['type'];
+		for(var j=start;j<=end;j++){
+			const answer = {no:j,type:objective.type,option:objective.option,answer:'A',subject:0};
+			this.objectiveAnswers.push(answer);
+		}
+	}	
+
+	removeAnswers(objective){
+		const answers = [];
+		
+		let start = objective['start'];
+		let end = objective['end'];
+		let type = objective['type'];
+		for(var j=0;j<this.objectiveAnswers.length;j++){
+			const answer = this.objectiveAnswers[j];
+			const no = answer['no'];
+			if(!(no>=start && no<=end && answer['type'] === type)){
+				answers.push(answer);
+			}
+		}
+		this.objectiveAnswers = answers;
+	}
+
+	addScore(objective){
+		let start = objective['start'];
+		let end = objective['end'];
+		let type = objective['type'];
+		let score = objective['score'];
+		let total = (end+1-start)*score;
+		if(type === 1){
+			this.radioScore = this.radioScore + total;
+		}else if(type === 2){
+			this.checkboxScore = this.checkboxScore + total;
+		}else if(type === 3){
+			this.judgmentScore = this.judgmentScore + total;
+		}
+		this.objectiveScore = this.objectiveScore + total;
+	}
+
+	removeScore(objective){
+		let start = objective['start'];
+		let end = objective['end'];
+		let type = objective['type'];
+		let score = objective['score'];
+		let total = (end+1-start)*score;
+		if(type === 1){
+			this.radioScore = this.radioScore - total;
+		}else if(type === 2){
+			this.checkboxScore = this.checkboxScore - total;
+		}else if(type === 3){
+			this.judgmentScore = this.judgmentScore - total;
+		}
+		this.objectiveScore = this.objectiveScore - total;
+	}
+
+	onValueChange(valueType,id){
+
+		const _objectives = [];
+		for(var i=0;i<this.objectives.length;i++){
+			const obj = this.objectives[i];
+			if(obj && obj['id'] === id){
+				let start = obj['start'];
+				let end = obj['end'];
+				let type = obj['type'];
+				let score = obj['score'];
+
+				this.removeScore(obj);
+				this.removeAnswers(obj);
+
+				if(valueType === 1){
+					start = Number(this.elementRef.nativeElement.querySelector('#start_' + id).value);
+					obj['start'] = start;					
+				}else if(valueType === 2){
+					end = Number(this.elementRef.nativeElement.querySelector('#end_' + id).value);
+					obj['end'] = end;
+				}else if(valueType === 3){
+					type = Number(this.elementRef.nativeElement.querySelector('#type_' + id).value);
+					obj['type'] = type;
+					if(type === 1 || type === 2){
+						obj['option'] = 4;
+					}else if(type === 3){
+						obj['option'] = 2;
+					}
+				}else if(valueType === 4){
+					const option = Number(this.elementRef.nativeElement.querySelector('#option_' + id).value);
+					obj['option'] = option;
+				}else if(valueType === 5){
+					score = Number(this.elementRef.nativeElement.querySelector('#score_' + id).value);
+					obj['score'] = score;
+				}
+
+				this.addScore(obj);
+				this.addAnswers(obj);
+			}
+			_objectives.push(obj);
+		}
+		this.objectives = _objectives;
+	}
+
 }
