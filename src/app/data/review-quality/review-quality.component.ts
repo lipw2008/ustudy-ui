17a832bd5a62e0538b7bca297a98e8f33ee43f0d
Jet <ijetlee@gmail.com>
2017-10-26 19:12:19 +0800

    review quality page of data center.

---
+++ src/app/data/review-quality/review-quality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-quality',
+  templateUrl: './review-quality.component.html',
+  styleUrls: ['./review-quality.component.css']
+})
+export class ReviewQualityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
