8744f63e3d59609d661cc60cfb4de936fa4f78ba
Jet <ijetlee@gmail.com>
2017-10-28 11:52:49 +0800

    schedule details page use data from api

--- src/app/data/schedule-details/schedule-details.component.ts
+++ src/app/data/schedule-details/schedule-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SharedService} from "../../shared.service";
 
 @Component({
   selector: 'app-schedule-details',
@@ -7,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleDetailsComponent implements OnInit {
   tab: String = 'subjectReview';
+  marks: any;
+  questionList = [];
 
-  constructor() { }
+  constructor(private _sharedService: SharedService) { }
 
   ngOnInit() {
+    this.reload()
+  }
+  reload(): void {
+    this._sharedService.makeRequest('GET', 'assets/api/exams/marklist.json', '').then((data: any) => {
+      console.log('data: ' + JSON.stringify(data));
+      this.marks = data;
+      for (const mark of this.marks) {
+        if (mark.markType === '标准') {
+          for (const s of mark.summary) {
+            s.teacherName = mark.teacherName
+          }
+          this.questionList = this.questionList.concat(mark.summary);
+        }
+      }
+    }).catch((error: any) => {
+      console.log(error.status);
+      console.log(error.statusText);
+    });
   }
 
+  getQuestionNames(mark: any ) {
+    let res;
+    res =  mark.summary.map((s) => {
+      return s.questionName
+    }).join(',');
+    return res
+  }
 }
