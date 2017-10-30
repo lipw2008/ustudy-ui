315092f9c3805223d47d447b5b1f97cf7a94fed1
Jet <ijetlee@gmail.com>
2017-10-30 12:56:16 +0800

    answer filter in questions analysis page.

---
+++ src/app/utils/answer-filter/answer-filter.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'answer-filter',
+  templateUrl: './answer-filter.component.html',
+  styleUrls: ['./answer-filter.component.css']
+})
+export class AnswerFilterComponent implements OnChanges {
+  @Input() answer: any;
+  @Output() selectResult = new EventEmitter();
+
+  constructor() { }
+
+  ngOnChanges() {
+    this.reload();
+  }
+
+  returnResult() {
+    this.selectResult.emit('')
+  }
+
+  reload() {
+  }
+
+}
