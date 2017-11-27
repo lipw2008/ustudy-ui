ade1083485a6e300f8b60d258d5bb6e7e6883631
Jet <ijetlee@gmail.com>
2017-11-27 13:49:16 +0800

    answer paper center

--- src/app/exam/answer-paper/answer-paper.module.ts
+++ src/app/exam/answer-paper/answer-paper.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnswerPaperListComponent } from './answer-paper-list/answer-paper-list.component';
 import { RouterModule } from '@angular/router';
-import {UtilsModule} from "../../utils/utils.module";
+import { UtilsModule } from '../../utils/utils.module';
+import { AnswerComponent } from './answer/answer.component';
+import { FormsModule } from '@angular/forms';
+import { AnswerService } from './answer.service';
 
 @NgModule({
   imports: [
     CommonModule,
     UtilsModule,
+    FormsModule,
     RouterModule.forChild([
       { path: 'answerPapers', component: AnswerPaperListComponent },
+      { path: 'answers', component: AnswerComponent },
     ]),
   ],
-  declarations: [AnswerPaperListComponent]
+  declarations: [AnswerPaperListComponent, AnswerComponent],
+  providers: [AnswerService]
 })
 export class AnswerPaperModule { }
