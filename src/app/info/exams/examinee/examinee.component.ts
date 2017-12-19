579f4d4a19066f52b6cd3abf43425c932cf9fce1
Jet <ijetlee@gmail.com>
2017-12-19 12:24:05 +0800

    delete/update examinee.

--- src/app/info/exams/examinee/examinee.component.ts
+++ src/app/info/exams/examinee/examinee.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {ExamService} from "../../../exam/exam.service";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {ActivatedRoute} from '@angular/router';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ExamService} from '../../../exam/exam.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-examinee',
@@ -30,6 +31,7 @@ export class ExamineeComponent implements OnInit {
   stuId: string;
   stuExamId: string;
   examineeClass: any;
+  examineeId: any;
 
 
   constructor(private route: ActivatedRoute, private _examService: ExamService, public fb: FormBuilder) {
@@ -58,14 +60,38 @@ export class ExamineeComponent implements OnInit {
     })
   }
 
-  addExaminee(modal) {
+  addOrUpdateExaminee(modal) {
     if (!this.name || !this.stuExamId || !this.examineeClass) {
       alert('请输入必填内容');
       return
     }
-    this._examService.addExaminee([{stuName: this.name, stuId: this.stuId, stuExamId: this.stuExamId, classId: this.examineeClass.classId}]).then((data) => {
-      alert('新建考生成功');
+    this._examService.addOrUpdateExaminee([{stuName: this.name, stuId: this.stuId, stuExamId: this.stuExamId, classId: this.examineeClass.classId,
+      examId: this.examId, gradeId: this.gradeId}]).then((data) => {
+      alert(`${this.examineeId ? '更新' : '新建'}考生成功`);
+      if (this.examineeId) {
+        const examinee = _.find(this.examinees, {studentId: this.examineeId});
+        examinee.studentName = this.name;
+        examinee.stuId = this.stuId;
+        examinee.stuExamId = this.stuExamId;
+        examinee.className = this.examineeClass.className;
+        examinee.classId = this.examineeClass.classId;
+      }
+      modal.hide()
     });
-    modal.hide()
+  }
+
+  deleteExaminee(examinee) {
+    this._examService.deleteExaminee(examinee.studentId).then((data) => {
+      alert('删除考生成功');
+      _.remove(this.examinees, examinee)
+    });
+  }
+
+  editExaminee(modal, examinee) {
+    this.name = examinee.studentName;
+    this.stuExamId = examinee.examCode;
+    this.examineeClass = _.find(this.classes, {classId: examinee.classId});
+    this.examineeId = examinee.studentId;
+    modal.show()
   }
 }
