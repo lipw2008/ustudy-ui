1608903fc8b448a476d343676e9b04b62bfe944d
Jet <ijetlee@gmail.com>
2017-12-28 19:02:16 +0800

    fix breadcrumb link of task assign and delete mock data.

--- src/app/exam/setanswers/selectsubject.component.ts
+++ src/app/exam/setanswers/selectsubject.component.ts
@@ -12,63 +12,9 @@ export class SelectSubjectComponent implements OnInit {
   errorMessage: string;
   examId: string;
   type = 'answers';
-
   isCreated = false;
-
-  exams = [
-    { id: '1', examName: '铜川一中16-17学年上学期期末考试1' },
-    { id: '2', examName: '铜川一中16-17学年上学期期末考试2' },
-    { id: '3', examName: '铜川一中16-17学年上学期期末考试3' },
-    { id: '4', examName: '铜川一中16-17学年上学期期末考试4' },
-    { id: '5', examName: '铜川一中16-17学年上学期期末考试5' },
-    { id: '6', examName: '铜川一中16-17学年上学期期末考试6' }
-  ];
-
-  gradesubjects = [
-    {
-      id: '10', grade: '高一', subjects: [
-        { id: '1', subName: '语文', answerSeted: true },
-        { id: '2', subName: '数学', answerSeted: false },
-        { id: '3', subName: '英语', answerSeted: true },
-        { id: '4', subName: '物理', answerSeted: false },
-        { id: '5', subName: '化学', answerSeted: true },
-        { id: '6', subName: '生物', answerSeted: false },
-        { id: '7', subName: '政治', answerSeted: true },
-        { id: '8', subName: '历史', answerSeted: false },
-        { id: '9', subName: '地理', answerSeted: true }
-      ]
-    },
-    {
-      id: '11', grade: '高二', subjects: [
-        { id: '1', subName: '语文', answerSeted: true },
-        { id: '2', subName: '数学', answerSeted: false },
-        { id: '3', subName: '英语', answerSeted: true },
-        { id: '4', subName: '物理', answerSeted: false },
-        { id: '5', subName: '化学', answerSeted: false },
-        { id: '6', subName: '生物', answerSeted: false },
-        { id: '7', subName: '政治', answerSeted: true },
-        { id: '8', subName: '历史', answerSeted: false },
-        { id: '9', subName: '地理', answerSeted: true },
-        { id: '10', subName: '文综', answerSeted: false },
-        { id: '11', subName: '理综', answerSeted: true }
-      ]
-    },
-    {
-      id: '12', grade: '高三', subjects: [
-        { id: '1', subName: '语文', answerSeted: true },
-        { id: '2', subName: '数学', answerSeted: false },
-        { id: '3', subName: '英语', answerSeted: true },
-        { id: '4', subName: '物理', answerSeted: false },
-        { id: '5', subName: '化学', answerSeted: true },
-        { id: '6', subName: '生物', answerSeted: false },
-        { id: '7', subName: '政治', answerSeted: false },
-        { id: '8', subName: '历史', answerSeted: false },
-        { id: '9', subName: '地理', answerSeted: false },
-        { id: '10', subName: '文综', answerSeted: true },
-        { id: '11', subName: '理综', answerSeted: false }
-      ]
-    }
-  ];
+  exams = [];
+  gradesubjects = [];
   public examSelected: boolean;
 
   constructor(private _sharedService: SharedService, public fb: FormBuilder, private elementRef: ElementRef,
